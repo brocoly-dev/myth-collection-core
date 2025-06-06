dba4a3cd05dee1e9c7a24bcf5d3bb0eec676f530
brocoly-dev <rivasarmando271084@icloud.com>
2025-06-06 16:31:42 -0600

    Added new property

--- src/main/java/com/mesofi/myth/collection/core/model/Figurine.java
+++ src/main/java/com/mesofi/myth/collection/core/model/Figurine.java
@@ -1,12 +1,33 @@
 package com.mesofi.myth.collection.core.model;
 
 /**
- * Represents a collectible figurine in the collection system. This class encapsulates the core
- * properties and behavior of a figurine item.
+ * Represents a figurine in the collection system. A figurine contains information about its base
+ * name, displayable name, associated line-up, and other properties that define a collectible
+ * figurine item.
  */
 public class Figurine {
+  private String baseName;
   private String displayableName; // This field is calculated ...
   private LineUp lineUp;
+  private boolean oce;
+
+  /**
+   * Gets the base name of the figurine.
+   *
+   * @return the base name of the figurine
+   */
+  public String getBaseName() {
+    return baseName;
+  }
+
+  /**
+   * Sets the base name of the figurine.
+   *
+   * @param baseName the base name to set for the figurine
+   */
+  public void setBaseName(String baseName) {
+    this.baseName = baseName;
+  }
 
   /**
    * Gets the displayable name of the figurine. This is a calculated field that provides a
@@ -44,5 +65,23 @@ public class Figurine {
    */
   public void setLineUp(LineUp lineUp) {
     this.lineUp = lineUp;
+  }
+
+  /**
+   * Gets the OCE (Original Color Edition) status of this figurine.
+   *
+   * @return true if this figurine is an OCE, false otherwise
+   */
+  public boolean isOce() {
+    return oce;
+  }
+
+  /**
+   * Sets the OCE (Original Color Edition) status of this figurine.
+   *
+   * @param oce true if this figurine is an OCE, false otherwise
+   */
+  public void setOce(boolean oce) {
+    this.oce = oce;
   }
 }
